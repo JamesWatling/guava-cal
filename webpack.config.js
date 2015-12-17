b7c531d8a92883421fd79342cc611772cdca8f98
James Watling <watling.james@gmail.com>
2015-12-17 17:56:55 +0800

    Start using react to build basic Calendar functionality. Currently renders a 35 day calendar using components

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+var CODE = __dirname+'/src';
+var React = require('react');
+
+makeIndex();
+
+module.exports = {
+
+  devtool: 'eval',
+
+  entry: fs.readdirSync(CODE).reduce(function (entries, dir) {
+    if (isDirectory(path.join(CODE, dir)))
+      entries[dir] = path.join(CODE, dir, 'app.js');
+    return entries;
+  }, {}),
+
+  output: {
+    path: 'src/__build__',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.js$/, loader: 'jsx-loader?harmony' }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('shared.js')
+  ]
+
+};
+
+function makeIndex () {
+  var list = fs.readdirSync(CODE).filter(function(dir) {
+    return isDirectory(path.join(CODE, dir));
+  }).map(function (dir) {
+    return React.DOM.li({}, React.DOM.a({href: '/'+dir}, dir.replace(/-/g, ' ')));
+  });
+  var markup = React.renderToStaticMarkup((
+    React.DOM.html({},
+      React.DOM.link({rel: 'stylesheet', href: '/shared.css'}),
+      React.DOM.body({id: "index"},
+        React.DOM.ul({}, list)
+      )
+    )
+  ));
+  fs.writeFileSync('./src/index.html', markup);
+}
+
+function isDirectory(dir) {
+  return fs.lstatSync(dir).isDirectory();
+}
